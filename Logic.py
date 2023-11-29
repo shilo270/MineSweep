85e6bdfa656f4c33cd220d6adb312224e0d17757
talf7 <talfischer7.tf@gmail.com>
2023-11-29 11:58:07 +0200

    bombs are in place

--- Logic.py
+++ Logic.py
@@ -9,6 +9,7 @@ class Window:
         self.root.title("MineSweep game")
         self.root.geometry("480x640")
         self.root.configure(bg="lightgray")
+        self.root.attributes('-fullscreen',True)
         self.GridSize = 20
         self.numberOfMines = 20
         self.list_Of_Mines = [[] for self.GridSize in range(self.GridSize)]
@@ -21,22 +22,20 @@ class Window:
         self.label = tk.Label(text="MineSweep", height=2, bg="lightgray",bd=5, fg="black", font=("Courier",44))
         self.label.pack(pady=0)
         self.label.place(x=90, y=10)
+        self.placeMines(100)
 
 
-    def placeMines(numberOfMines):
-        ListPlacedMines = []
+    def placeMines(self,numberOfMines):
+        listPlacedMines = []
         i = 0
         while i <= numberOfMines:
-            x = random.randint(0, window.numberOfMines)
-            y = random.randint(0, window.numberOfMines)
-            for j in range(len(ListPlacedMines)):
-                if ListPlacedMines(j) == (x, y):
-                    break
-                if j == len(ListPlacedMines) and ListPlacedMines(j) != (x, y):
-                    ListPlacedMines.append((x, y))
-                    Button.self.list_Of_Mines[x][y].placedMine = True
-                    print(ListPlacedMines)
-                    i += 1
+            x = random.randint(0, self.GridSize - 1)
+            y = random.randint(0, self.GridSize - 1)
+            if (x,y) in listPlacedMines:
+                continue
+            listPlacedMines.append((x, y))
+            self.list_Of_Mines[x][y].placedMine = True
+            i += 1
     # easy 9x9 number of mines 10
     # intermediate 16x16 number of mines 40
     # expert 16x30 number of mines 99
@@ -45,18 +44,21 @@ class Window:
 class Button:
     def __init__(self,root):
         self.root = root
-        self.button = tk.Button(root,height=1, width=2)
+        self.button = tk.Button(root,command=self.pressedButton,height=1, width=2)
         self.placedMine = False
 
     def pressedButton(self):
-        if Button.placedMine == True:
-            Window.label = tk.Label(text="You lost, game over", height=2, bg="lightgray", bd=5, fg="black",
-                                  font=("Courier", 44))
-            Window.label.pack(pady=0)
-            Window.label.place(x=90, y=13)
+        msg = tk.Label(root, text="", height=2, bg="lightgray", bd=5, fg="black",
+                                font=("Courier", 44))
+        msg.pack(pady=0)
+        msg.place(x=90, y=13)
+        if self.placedMine is True:
+            msg.configure(text ="You lost, game over")
             # game over need work
         else:
-            if Button.placedMine == False:
+            msg.configure(text="keep playing your good")
+
+
                 
 
 
