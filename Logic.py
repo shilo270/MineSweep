24cd74fb17a6ad9ec94a413df0512d3d4e553eb9
shilo270 <shinigami270@gmail.com>
2023-11-29 13:39:46 +0200

    add pictures

--- Logic.py
+++ Logic.py
@@ -1,6 +1,6 @@
 import time
 import tkinter as tk
-#from PIL import Image,ImageTk
+from PIL import Image,ImageTk
 import random
 
 class Window:
@@ -9,9 +9,8 @@ class Window:
         self.root.title("MineSweep game")
         self.root.geometry("480x640")
         self.root.configure(bg="lightgray")
-        self.root.attributes('-fullscreen',True)
-        self.GridSize = 20
-        self.numberOfMines = 20
+        #self.root.attributes('-fullscreen',True)
+        self.GridSize = 5
         self.list_Of_Mines = [[] for self.GridSize in range(self.GridSize)]
         #self.canvas = Canvas(bg="pink", height="200")
         for i in range(self.GridSize):
@@ -22,7 +21,7 @@ class Window:
         self.label = tk.Label(text="MineSweep", height=2, bg="lightgray",bd=5, fg="black", font=("Courier",44))
         self.label.pack(pady=0)
         self.label.place(x=90, y=10)
-        self.placeMines(100)
+        self.placeMines(5)
 
 
     def placeMines(self,numberOfMines):
@@ -46,23 +45,24 @@ class Button:
         self.root = root
         self.button = tk.Button(root,command=self.pressedButton,height=1, width=2)
         self.placedMine = False
+        self.MineImage =ImageTk.PhotoImage(Image.open('pngwing.png').resize((13,13)))
+
 
     def pressedButton(self):
-        msg = tk.Label(root, text="", height=2, bg="lightgray", bd=5, fg="black",
-                                font=("Courier", 44))
+        msg = tk.Label(root, text="", height=2, bg="lightgray", bd=5, fg="black", font=("Courier", 44))
         msg.pack(pady=0)
         msg.place(x=90, y=13)
         if self.placedMine is True:
             msg.configure(text ="You lost, game over")
+            self.button.configure(image=self.MineImage,height=20, width=18,bg="red")
             # game over need work
         else:
             msg.configure(text="keep playing your good")
-
-
+            for i in range(9):
                 
+            self.button.configure(text="3",height=1, width=2)
 
 
 root = tk.Tk()
-#Window.placeMines(10)
 window = Window(root)
 root.mainloop()
